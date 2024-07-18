d163c67b0bc72513d81e07c2160a17126e6f2396
Jgalindom03 <jgalindom2003@gmail.com>
2024-07-18 19:29:04 +0200

    updated

---
+++ islands/CheckOutButton.tsx
@@ -0,0 +1,14 @@
+const CheckOutButton = () => (
+    <button
+      class="checkout-button"
+      onClick={() => {
+        // got to /checkout page
+        window.location.href = "/checkout";
+      }}
+    >
+      Checkout
+    </button>
+  );
+  
+  export default CheckOutButton;
+  
---
+++ routes/cart.tsx
@@ -0,0 +1,22 @@
+import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
+import Cart from "../components/Cart.tsx";
+import { CartItem } from "../types.ts";
+import { getCookies } from "$std/http/cookie.ts";
+
+type Data={
+    cart:CartItem[];
+    
+}
+export const handler: Handlers={
+    GET:(req:Request, ctx:FreshContext<unknown, Data>)=>{
+        const cookies= getCookies(req.headers);
+        const cart= JSON.parse(cookies.cart);
+        return ctx.render({cart});
+    }
+}
+const Page = (props:PageProps<Data>) => {
+    const cart= props.data.cart;
+    return <Cart cart={cart}/>
+};
+
+export default Page;
